6c68249a583bd73cacb3dfee986f6e61fc6412c4
Bach1685 <alekseimatyuhin@gmail.com>
2021-10-15 00:22:46 +0300

    feat: add script

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu = document.getElementsByClassName('burger');
+
+menu[0].addEventListener("click", () => {
+    document.getElementsByClassName('menu')[0].style = 
+    "display: flex;"
+    });
+
