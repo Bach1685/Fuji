612e969417b0449defc21d3b8481fd793ddb57b3
Bach1685 <alekseimatyuhin@gmail.com>
2021-10-20 00:01:28 +0300

    feat: menu

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let menu = document.getElementsByClassName('burger');
 
 menu[0].addEventListener("click", () => {
-    document.getElementsByClassName('menu')[0].style = 
+    document.getElementsByClassName('cross-menu')[0].style = 
     "display: flex;" + 
-    "left: 0;" + 
-    "padding-left: 0;"
-    // "background-color: black";
+    "left: 0px;" + 
+    "top: 0px;" +
+    "padding-left: 0;";
 
+    // "background-color: black";
+    document.getElementsByClassName('cross')[0].style = 
+    "display: block;";
     });
 
