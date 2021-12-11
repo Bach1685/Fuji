8a4a771ca74ffdcdd62bb67812b404c73861769d
Bach1685 <alekseimatyuhin@gmail.com>
2021-12-11 23:20:48 +0300

    feat: внедняем коэффициент пропорциональности

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-document
-.getElementById('burger')
-.addEventListener("click", () => {
-    document.getElementById('cross-menu')
-    .style = 
-    "left: 0px;";
-    });
+const screenWidth = window.innerWidth;
+console.log(screenWidth);
 
-Array.from(document.getElementsByClassName('close-menu'))
-.forEach(element => {
-    element.addEventListener("click", () => {
-        document.getElementById('cross-menu')
-        .style = 
-        "left: -490px;" +
-        "display: block;";
-        });
-});
+if(screenWidth < 1540){
+  document.documentElement.style.setProperty("--size-coeff", "0.7");
+} else{
+  document.documentElement.style.setProperty("--size-coeff", "1");
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    let burgerStyle = document.getElementById("burger").style;
-  if (document.body.scrollTop > 50 
-    || document.documentElement.scrollTop > 50) {
-        burgerStyle.paddingTop = "10px";
-        burgerStyle.paddingLeft = "10px";
-  } else {
-    burgerStyle.paddingTop = "35px";
-    burgerStyle.paddingLeft = "20px";
-  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// document
+// .getElementById('burger')
+// .addEventListener("click", () => {
+//     document.getElementById('cross-menu')
+//     .style = 
+//     "left: 0px;";
+//     });
+
+// Array.from(document.getElementsByClassName('close-menu'))
+// .forEach(element => {
+//     element.addEventListener("click", () => {
+//         document.getElementById('cross-menu')
+//         .style = 
+//         "left: -490px;" +
+//         "display: block;";
+//         });
+// });
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     let burgerStyle = document.getElementById("burger").style;
+//   if (document.body.scrollTop > 50 
+//     || document.documentElement.scrollTop > 50) {
+//         burgerStyle.paddingTop = "10px";
+//         burgerStyle.paddingLeft = "10px";
+//   } else {
+//     burgerStyle.paddingTop = "35px";
+//     burgerStyle.paddingLeft = "20px";
+//   }
+// }
 
 
