345bafb8016af00b35518ef96ff1460aabe472cd
Bach1685 <alekseimatyuhin@gmail.com>
2022-01-06 01:08:46 +0300

    адаптируем

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-const screenWidth = window.innerWidth;
-console.log(String(1920 / screenWidth));
-const borderScreenWidth = 1300;
-if (screenWidth >= borderScreenWidth) {
+document.getElementById("burger-img").addEventListener("click", () => {
+  document.getElementById("burger").style = "left: 0px;";
+});
+
+Array.from(document.getElementsByClassName("close-burger-menu")).forEach(
+  (element) => {
+    element.addEventListener("click", () => {
+      document.getElementById("burger").style = "left: -300px;";
+    });
+  }
+);
+
+const currentScreenWidth = window.innerWidth;
+const startScreenWidth = 1920;
+const borderScreenWidth = 1280;
+if (currentScreenWidth >= borderScreenWidth) {
   document.documentElement.style.setProperty(
     "--size-coeff",
-    String(screenWidth / 1920)
+    String(currentScreenWidth / startScreenWidth)
   );
+} else if (currentScreenWidth < 768) {
+  document.documentElement.style.setProperty("--size-coeff", String(0.55));
 } else {
   document.documentElement.style.setProperty(
     "--size-coeff",
-    String(screenWidth / borderScreenWidth)
+    String(borderScreenWidth / startScreenWidth)
   );
 }
-
-// document
-// .getElementById('burger')
-// .addEventListener("click", () => {
-//     document.getElementById('cross-menu')
-//     .style =
-//     "left: 0px;";
-//     });
-
-// Array.from(document.getElementsByClassName('close-menu'))
-// .forEach(element => {
-//     element.addEventListener("click", () => {
-//         document.getElementById('cross-menu')
-//         .style =
-//         "left: -490px;" +
-//         "display: block;";
-//         });
-// });
 
 // window.onscroll = function() {scrollFunction()};
 
